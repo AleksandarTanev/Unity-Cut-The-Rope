c84ee49c77bb0be8dbd8f9a7b37a8293aa0c135b
AleksandarTanev <aleksander.tanev.51@gmail.com>
2018-06-14 01:03:43 +0300

    Update to the logic

---
+++ Assets/Scripts/Rope.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+public class Rope : MonoBehaviour 
+{
+    public Weight weight;
+    public HingeJoint2D hook;
+    public GameObject linkPrefab;
+
+    public int links = 7;
+
+    private void Start()
+    {
+        GenerateRope();
+    }
+
+    private void GenerateRope()
+    {
+        Rigidbody2D previousLink = hook.GetComponent<Rigidbody2D>();
+        for (int i = 0; i < links; i++)
+        {
+            GameObject link = Instantiate(linkPrefab, this.transform);
+
+            link.GetComponent<HingeJoint2D>().connectedBody = previousLink;
+
+            if (i < links - 1)
+            {
+                previousLink = link.GetComponent<Rigidbody2D>();
+            }
+            else
+            {
+                weight.ConnectRopeEnd(link.GetComponent<Rigidbody2D>());
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Weight.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Weight : MonoBehaviour 
+{
+    public float distanceFromChainEnd = -0.7f;
+
+	public void ConnectRopeEnd(Rigidbody2D endRB)
+    {
+        HingeJoint2D joint = gameObject.AddComponent<HingeJoint2D>();
+        joint.connectedBody = endRB;
+        joint.autoConfigureConnectedAnchor = false;
+        joint.anchor = Vector2.zero;
+        joint.connectedAnchor = new Vector2(0f, distanceFromChainEnd);
+    }
+}
