b0b765e17410ed1b678befea20fb6cc79f033d3d
AleksandarTanev <a.tanev@xs-software.com>
2018-06-19 17:41:45 +0300

    Added textmesh pro

---
+++ Assets/Scripts/GuiManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GuiManager : MonoBehaviour 
+{
+    public TextMeshProUGUI info;
+
+    private void Start()
+    {
+        var componentsOnTheScene = Component.FindObjectsOfType<Rope>();
+
+        info.text = "Ropes: "  + componentsOnTheScene.Length.ToString();
+    }
+}
