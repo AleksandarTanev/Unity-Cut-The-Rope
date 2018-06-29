26dfa2c7333c3adc6e4732d87f445bfd12397d07
AleksandarTanev <a.tanev@xs-software.com>
2018-06-29 18:27:51 +0300

    Some changes

--- Assets/Scripts/GuiManager.cs
+++ Assets/Scripts/GuiManager.cs
@@ -4,12 +4,20 @@ using TMPro;
 
 public class GuiManager : MonoBehaviour 
 {
-    public TextMeshProUGUI info;
+    public TextMeshProUGUI textMesh;
+    public Text normalText;
 
     private void Start()
     {
         var componentsOnTheScene = Component.FindObjectsOfType<Rope>();
 
-        info.text = "Ropes: "  + componentsOnTheScene.Length.ToString();
+        // info.text = "Ropes: "  + componentsOnTheScene.Length.ToString();
+        char symbol_1 = '\x77';
+        char symbol_2 = '\x2377';
+        textMesh.text = symbol_1.ToString() + " " + symbol_2.ToString();
+        normalText.text = symbol_1.ToString() + " " + symbol_2.ToString();
+
+        textMesh.text = textMesh.text.Replace(symbol_2.ToString(), "1");
+        normalText.text = normalText.text.Replace(symbol_2.ToString(), "1");
     }
 }
